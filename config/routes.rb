df0e0922c497bccbfc3d076b1c153100b99a40d2
Tatiana Parfait <tania.parfait@hotmail.fr>
2023-08-15 15:10:58 +0200

    kawai liste des tahces

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,21 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+
+  # add a new task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
   # root "articles#index"
+  get 'tasks', to: 'tasks#index'
+
+  # details tasks
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # update task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # destroy a task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy_task
 end
